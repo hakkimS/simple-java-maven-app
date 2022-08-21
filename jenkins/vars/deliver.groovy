c534132d9ec03c6b57cd197885c77df06ceb4901
hakkimS <84006367+hakkimS@users.noreply.github.com>
2022-08-21 12:02:22 +0530

    Update deliver.groovy

--- jenkins/vars/deliver.groovy
+++ jenkins/vars/deliver.groovy
@@ -1,5 +1,5 @@
 def call(){
-sh """#!/usr/bin/env bash
+sh ''' #!/usr/bin/env bash
 
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
@@ -24,5 +24,5 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar"""
+java -jar target/${NAME}-${VERSION}.jar '''
 }
